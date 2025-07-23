2902ea580f62bee32dd1869ca6eb6026bb74867c
Greg Walker <git@greg.email>
2025-07-23 16:02:27 -0500

    18f version

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import fastify from "fastify";
 import { initCache, getLink, getRandom } from "./links.js";
 
@@ -33,8 +35,19 @@ const main = async () => {
     response.redirect(links.previous.url, 302);
   });
 
+  server.get("/webcomponent.js", async (_, response) => {
+    response.header("Content-Type", "text/javascript");
+    response.send(
+      await fs.readFile(path.join(import.meta.dirname, "webcomponent.js")),
+    );
+  });
+
   await server.listen({ port, host: "0.0.0.0" });
   console.log(`listening on port ${port}`);
+
+  process.on("SIGINT", () => {
+    process.exit(0);
+  });
 };
 
 main();
